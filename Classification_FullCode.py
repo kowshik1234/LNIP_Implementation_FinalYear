307c61cedff200e59f386fb51058e2a7f7d855f0
kowshik <kowshik700@gmail.com>
2020-02-12 20:57:07 +0530

    Update Classification_FullCode.py

--- Classification_FullCode.py
+++ Classification_FullCode.py
@@ -16,65 +16,85 @@ from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV 
 from sklearn.kernel_approximation import RBFSampler
 from sklearn.linear_model import SGDClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+
 import warnings
 warnings.filterwarnings("ignore")
 
 
-x = pd.read_csv(r'F:\4th_Year_project_files\Dataset_DDSM_database\LNIP_super_all.csv')
+x = pd.read_csv(r'F:\4th_Year_project_files\Dataset_DDSM_database\LBP_all.csv')
 
 training_set,test_set = train_test_split(x,test_size=0.2,random_state=0)
-X_train = training_set.iloc[:,0:512].values
-Y_train = training_set.iloc[:,512].values
-X_test = test_set.iloc[:,0:512].values
-Y_test = test_set.iloc[:,512].values
+X_train = training_set.iloc[:,0:59].values
+Y_train = training_set.iloc[:,59].values
+X_test = test_set.iloc[:,0:59].values
+Y_test = test_set.iloc[:,59].values
 
-classifier = SVC(C=1000, cache_size=200, class_weight=None, coef0=0.0,
-    decision_function_shape='ovr', degree=3, gamma=10, kernel='rbf',
-    max_iter=-1, probability=False, random_state=None, shrinking=True,
-    tol=0.001, verbose=False)
+#################MLP Classifier#################
+classifier = MLPClassifier(hidden_layer_sizes=(150,100,50), max_iter=300,activation = 'relu',solver='adam',random_state=1)
+classifier.fit(X_train, Y_train)
+y_pred = classifier.predict(X_test)
+cm = confusion_matrix(y_pred, Y_test)
+accuracy = float(cm.diagonal().sum())/len(Y_test)
+print("Accuracy of MLP classifier: ",accuracy*100)
 
+#################Optimized SVM with RBF Kernel###############
+#For getting 98% accuracy feed C=1100 and Gamma = 1000
+
+classifier = SVC(C=1100, cache_size=200, class_weight=None, coef0=0.0,
+    decision_function_shape='ovr', degree=3, gamma=1000, kernel='rbf', max_iter=-1,
+    probability=False, random_state=None, shrinking=True, tol=0.001,
+    verbose=False)
 classifier.fit(X_train,Y_train)
 SVM_pred = classifier.predict(X_test)
 cm = confusion_matrix(Y_test,SVM_pred)
 accuracy = float(cm.diagonal().sum())/len(Y_test)
-print("\nAccuracy Of Linear SVM For The Given Dataset: ", accuracy)
+print("\nAccuracy Of Optimized SVM with RBF kernel "+str(accuracy*100))#+" by optimized RBF with 'C'= "+str(1100)+" Gamma value= "+str(1000))
 
-#########################Linear SVM Classifier###############
-##SVM = svm.LinearSVC()
-##SVM.fit(X_train,Y_train)
-##SVM_pred = SVM.predict(X_test)
-##cm = confusion_matrix(Y_test,SVM_pred)
-##accuracy = float(cm.diagonal().sum())/len(Y_test)
-##print("\nAccuracy Of Linear SVM For The Given Dataset: ", accuracy)
-##
-#########################SVM with Radial Basis Function Kernel##############
-##classifier = SVC(kernel='rbf', random_state = 1)
-##classifier.fit(X_train,Y_train)
-##rbf_predict = classifier.predict(X_test)
-##cm = confusion_matrix(Y_test,rbf_predict)
-##accuracy = float(cm.diagonal().sum())/len(Y_test)
-##print("\nAccuracy Of Radial Basis Function For The Given Dataset : ", accuracy)
-##
-######################### Random Forest Classifier#################
+########################Linear SVM Classifier###############
+SVM = svm.LinearSVC()
+SVM.fit(X_train,Y_train)
+SVM_pred = SVM.predict(X_test)
+cm = confusion_matrix(Y_test,SVM_pred)
+accuracy = float(cm.diagonal().sum())/len(Y_test)
+print("\nAccuracy Of Linear SVM For The Given Dataset: ", accuracy*100)
+
+##############################Naive Bayes################
+gnb = GaussianNB() 
+gnb.fit(X_train, Y_train) 
+y_pred = gnb.predict(X_test) 
+cm = confusion_matrix(Y_test,y_pred)
+accuracy = float(cm.diagonal().sum())/len(Y_test)
+#print("\nAccuracy of Naive Bayes Classsifier:", accuracy*100)
+
+######################## Random Forest Classifier#################
 rfc = RandomForestClassifier()
 rfc.fit(X_train,Y_train)
-
-#########################Making Predictions##################
 rfc_predict = rfc.predict(X_test)
 cm = confusion_matrix(Y_test,rfc_predict)
 accuracy = float(cm.diagonal().sum())/len(Y_test)
-print("\nAccuracy Of Random Forest For The Given Dataset :", accuracy)
-######################### XG Boost############################
+print("\nAccuracy Of Random Forest For The Given Dataset :", accuracy*100)
+
+###################### XG Boost############################
 model = XGBClassifier()
 model.fit(X_train, Y_train)
-# make predictions for test data
+#make predictions for test data
 y_pred = model.predict(X_test)
-##cm = confusion_matrix(Y_test,y_pred)
-##accuracy = float(cm.diagonal().sum())/len(Y_test)
-##print("Accuracy is ->",accuracy)
-
+cm = confusion_matrix(Y_test,y_pred)
+accuracy = float(cm.diagonal().sum())/len(Y_test)
 predictions = [round(value) for value in y_pred]
-# evaluate predictions
+#evaluate predictions
 accuracy = accuracy_score(Y_test, predictions)
-print("Accuracy: %.2f%%" % (accuracy * 100.0))
+print("\nAccuracy of XGBoost: %.2f%%" % (accuracy * 100.0))
 
+###################### Naive Bayes ########################
+gnb = GaussianNB()
+gnb.fit(X_train, Y_train)
+y_pred = gnb.predict(X_test)
+cm = confusion_matrix(Y_test,y_pred)
+accuracy = float(cm.diagonal().sum())/len(Y_test)
+predictions = [round(value) for value in y_pred]
+#evaluate predictions
+accuracy = accuracy_score(Y_test, predictions)
+print("\nAccuracy of Naive Bayes: %.2f%%" % (accuracy * 100.0))
